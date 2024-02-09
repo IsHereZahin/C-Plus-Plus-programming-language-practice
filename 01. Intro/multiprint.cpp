f5fe6588784ee243657131ddee08ee958f0f5b13
zahin <abuzahinmohammadnowsin@gmail.com>
2024-02-09 18:43:35 +0600

    Multiple printing and line breaks

---
+++ 01. Intro/multiprint.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout <<"Hello, "    <<"I'm " <<"\"Abu Zahin Mohammad Nowsin.\"\n";
+    cout <<"Iam a" <<" programmer" <<endl <<"from Bangladesh.";
+    return 0;
+}
