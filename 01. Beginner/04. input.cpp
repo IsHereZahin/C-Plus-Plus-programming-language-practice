ae1d20534bfa91f5b15a1dc4cea5dba3c4ed0614
zahin <abuzahinmohammadnowsin@gmail.com>
2024-02-09 19:42:28 +0600

    Input data

---
+++ 01. Beginner/04. input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int sum, num1, num2;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    sum = num1 + num2; // Calculate the sum after reading input from the user
+
+    cout << "The total number is: " << sum;
+
+    return 0;
+}
