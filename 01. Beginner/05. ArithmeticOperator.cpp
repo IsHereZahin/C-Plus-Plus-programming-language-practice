8db22a980b7134189e43a6a74929fd8911512170
zahin <abuzahinmohammadnowsin@gmail.com>
2024-02-10 14:54:14 +0600

    Arithmetic operator

---
+++ 01. Beginner/05. ArithmeticOperator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // A, a                 ----> Operands
+    // +, -, *, /, %, =     ----> Operator
+    // 1, 2, 3, 4, 5, 6, 7  ----> Constant
+
+    int num1, num2;
+    
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    cout << "Summation is: "     << num1 + num2 << endl;
+
+    cout << "Subtraction is: "   << num1 - num2 << endl;
+
+    cout << "Multiplication is: "<< num1 * num2 << endl;
+
+    cout << "Division is: "      << (float) num1 / num2 << endl;   //Type casting (int -> float)
+
+    cout << "Remainder is: "     << num1 % num2 << endl;
+    return 0;
+}
