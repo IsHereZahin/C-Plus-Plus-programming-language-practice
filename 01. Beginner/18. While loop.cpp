fce88557b35923b64f192cb3d21b4caec87211d9
zahin <abuzahinmohammadnowsin@gmail.com>
2024-02-16 20:40:10 +0600

    while loop

---
+++ 01. Beginner/18. While loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Using a while loop to print numbers from 1 to 10
+    int i = 1;
+    while (i <= 10)
+    {
+        cout << i << endl;
+        i++;
+    }
+    cout << "End of while loop" << endl;
+
+    return 0;
+}
