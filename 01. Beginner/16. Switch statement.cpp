f7c9b23a18c92ab9da98de093963d0cbfca636a4
zahin <abuzahinmohammadnowsin@gmail.com>
2024-02-16 15:50:22 +0600

    switch statement

---
+++ 01. Beginner/16. Switch statement.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int choice;
+
+    cout << "Simple Calculator\n";
+    cout << "1. Addition\n";
+    cout << "2. Subtraction\n";
+    cout << "3. Multiplication\n";
+    cout << "4. Division\n";
+    cout << "Enter your choice (1-4): ";
+    cin >> choice;
+
+    double num1, num2, result;
+
+    switch(choice) {
+        case 1:
+            cout << "Enter two numbers to add: ";
+            cin >> num1 >> num2;
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+            break;
+        case 2:
+            cout << "Enter two numbers to subtract: ";
+            cin >> num1 >> num2;
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+            break;
+        case 3:
+            cout << "Enter two numbers to multiply: ";
+            cin >> num1 >> num2;
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+            break;
+        case 4:
+            cout << "Enter two numbers to divide: ";
+            cin >> num1 >> num2;
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << result << endl;
+            } else {
+                cout << "Error! Division by zero is not allowed." << endl;
+            }
+            break;
+        default:
+            cout << "Invalid choice!" << endl;
+            break;
+    }
+
+    return 0;
+}
