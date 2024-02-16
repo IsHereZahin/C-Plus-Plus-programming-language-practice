44f61d15d6b778e66002cd0c05cba27edb6590c3
zahin <abuzahinmohammadnowsin@gmail.com>
2024-02-16 20:45:37 +0600

    Do while loop

---
+++ 01. Beginner/19. Do while loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Using a while loop to print numbers from 1 to 10
+    int i = 1;
+    do {
+        cout << i << endl;
+        i++;
+
+    } while (i <= 10);
+    
+    cout << "End of Do while loop" << endl;
+
+    return 0;
+}
