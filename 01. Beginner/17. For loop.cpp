0c9a30b110dc5198d84f1299fa99dfb79e2ddc48
zahin <abuzahinmohammadnowsin@gmail.com>
2024-02-16 16:05:55 +0600

    For loop

---
+++ 01. Beginner/17. For loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Using a for loop to print numbers from 1 to 10
+    for (int i = 1; i <= 10; ++i) {
+        cout << "Bangladesh " << i << endl;
+    }
+
+    return 0;
+}
