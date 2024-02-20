f1db8e754f11b61e0c6a844eb393f7216d86b471
DeveloperZahin <abuzahinmohammadnowsin@gmail.com>
2024-02-20 16:10:03 +0600

    2D Array loop print

--- 01. Beginner/23. 2D Array.cpp
+++ 01. Beginner/23. 2D Array.cpp
@@ -9,5 +9,17 @@ int main() {
                     {9, 10, 11, 12}
                   };
 
-    cout << A[0][0] << " " << A[1][0];
+    // cout << A[0][0] << " " << A[1][0];
+    cout << "2D Array Print:" << endl;
+
+    for (int row = 0; row < 3; row++)
+    {
+      for (int col = 0; col < 4; col++)
+      {
+        cout << " " << A[row][col];
+      }
+      cout << endl;
+    }
+    
+    return 0;
 }
