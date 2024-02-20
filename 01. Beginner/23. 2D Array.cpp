c5dc8800ab7101602f7266c3fb3f9a575ccf68d0
DeveloperZahin <abuzahinmohammadnowsin@gmail.com>
2024-02-20 15:53:48 +0600

    2D Array

---
+++ 01. Beginner/23. 2D Array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A[3][4] = { 
+                    {1, 2, 3, 4},
+                    {5, 6, 7, 8},
+                    {9, 10, 11, 12}
+                  };
+
+    cout << A[0][0] << " " << A[1][0];
+}
