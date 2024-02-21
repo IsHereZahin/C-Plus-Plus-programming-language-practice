faaefedf5742685211b8e2236823d61aded53bd9
DeveloperZahin <abuzahinmohammadnowsin@gmail.com>
2024-02-21 09:40:41 +0600

    Adding 2 Numbers Using Pointer

---
+++ 01. Beginner/26. Adding 2 Numbers Using Pointer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1 = 10, num2 = 20;
+
+    int *p1, *p2;
+
+    p1 = &num1;
+    p2 = &num2;
+
+    int sum = *p1 + *p2;
+
+    cout << sum << endl;
+    return 0;
+}
