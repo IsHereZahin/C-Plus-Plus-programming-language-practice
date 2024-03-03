30600d9c4819897645f084626af4a42f2e803bc8
DeveloperZahin <abuzahinmohammadnowsin@gmail.com>
2024-03-03 19:02:02 +0600

    String input

---
+++ 01. Beginner/37. String input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string name;
+
+  cout << "Enter your name: ";
+  getline(cin, name);
+
+  cout << "Welcome, " << name << endl;
+  return 0;
+}
